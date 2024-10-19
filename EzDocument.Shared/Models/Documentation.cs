5dca84ce83feb14512a3a2dac3516f7494aab553
nedlarry <lrrchinedu@gmail.com>
2024-10-19 06:15:13 +0100

    added commands and model

---
+++ EzDocument.Shared/Models/Documentation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzDocument.Shared.Models
+{
+    public class Documentation
+    {
+        public string BaseUrl { get; set; } = null!;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Version { get; set; } = "1.00";
+
+        public List<DocumentationEndpoint> Endpoints { get; set; } = new List<DocumentationEndpoint>();
+    }
+}
---
+++ EzDocument.Shared/Models/DocumentationEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzDocument.Shared.Models
+{
+    public class DocumentationEndpoint
+    {
+        public string EndpointUrl { get; set; } = null!;
+
+        public object Payload { get; set; } = null!;
+        public object SuccessResponsePayload { get; set; } = null!;
+        public object ErrorResponsePayload { get; set; } = null!;
+    }
+}
