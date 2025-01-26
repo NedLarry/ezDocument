1510db41dead9c52f4b351fbb94abb7a62caf26e
nedlarry <lrrchinedu@gmail.com>
2025-01-26 16:14:11 +0100

    local commit

--- EzDocument/obj/Debug/net8.0/EzDocument.AssemblyInfo.cs
+++ EzDocument/obj/Debug/net8.0/EzDocument.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EzDocument")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c16a7f167ce271b27008a40f87f90ffd86d77c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3f29faf44827b370b477a1aae8165457bd4b61")]
 [assembly: System.Reflection.AssemblyProductAttribute("EzDocument")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EzDocument")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
